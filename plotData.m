0c5880c183aa03b640a24feea9f59c679f053ac7
pziq <77043624+pziq@users.noreply.github.com>
2021-01-20 21:08:41 +0530

    Update plotData.m

--- plotData.m
+++ plotData.m
@@ -1,6 +1,6 @@
 function plotData(x, y)
 figure;
 plot(x, y, 'rx', 'MarkerSize', 10); % Plot the data
-  ylabel('Profit in $10,000s'); % Set the y-axis label
-  xlabel('Population of City in 10,000s'); % Set the x-axis label
+  ylabel('Marks obtained'); % Set the y-axis label
+  xlabel('Time in Hrs'); % Set the x-axis label
   end
