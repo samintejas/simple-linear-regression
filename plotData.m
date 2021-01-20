ee4cea83bacce5d65f0568363f8e3b53a52be720
pziq <77043624+pziq@users.noreply.github.com>
2021-01-20 20:52:36 +0530

    Create plotData.m

---
+++ plotData.m
@@ -0,0 +1,6 @@
+function plotData(x, y)
+figure;
+plot(x, y, 'rx', 'MarkerSize', 10); % Plot the data
+  ylabel('Profit in $10,000s'); % Set the y-axis label
+  xlabel('Population of City in 10,000s'); % Set the x-axis label
+  end
